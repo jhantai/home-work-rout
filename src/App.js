17068f1992b8ea20b89c3cc440ce4caeca1c48da
jhantai <tylerbroceva123@gmail.com>
2023-11-18 18:10:35 +0600

    test3

--- src/App.js
+++ src/App.js
@@ -9,75 +9,10 @@ import WeatherPage from "./pages/Weather/WeatherPage";
 
 function App() {
 
-
-        const API_KEY = '317147e078c54d6c882102156230710'
-        const [city, SetCity] = useState('')
-        const [weather, setWeather] = useState({})
-        const [isLoading, setIsLoading] = useState(false)
-        const [isActive, setIsActive] = useState(false)
-        const [gradus,setGradus] = useState ("")
-        const [wind_kph,setWind_kph] = useState ("")
-        const [ wind_degree,setWind_degree] = useState("")
-        const [errorText  , setErrorText]  = useState("")
-
-        const defaultState = () => {
-            setWeather(null)
-            setIsActive (false)
-            SetCity("")
-            setGradus('')
-            setWind_kph('')
-            setWind_degree('')
-            setErrorText ('')
-        }
-
-        const getWeather = async () => {
-            setIsLoading(true)
-            try {
-                const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
-                const data = await response.json()
-
-                if (response.status === 200) {
-                    setWeather(data)
-                    setIsLoading(false)
-                    setIsActive(true)
-                    setGradus("°C🌡")
-                    setWind_kph("Wind_kph")
-                    setWind_degree('wind_degree')
-                    setErrorText('')
-                    setWeather(data)
-                } else if(response.status === 400 ) {
-                    defaultState()
-                    setErrorText('В запросе синтаксическая ошибка')
-                } else {
-                    defaultState()
-                    setErrorText('Произошла неизвестная ошибка')
-                }
-            } catch (e) {
-                defaultState()
-                setErrorText('Сервер недоступен!')
-            } }
-
-        const resetWeather = async ()  => {
-            defaultState()
-        }
-
         return (
             <div className="app1">
-
-                <WeatherPage
-                    SetCity={SetCity}
-                    getWeather={getWeather}
-                    weather={weather}
-                    isLoading={isLoading}
-                    isActive={isActive}
-                    resetWeather={resetWeather}
-                    gradus = {gradus}
-                    wind_kph = {wind_kph}
-                    wind_degree = {wind_degree}
-                    errorText = {errorText}
-                />
-
                 <div className="App">
+                    {/*test*/}
                     <Routes>
                         <Route exact path="/" element={<HomePage />} />
                         <Route exact path="/weather" element={<WeatherPage />} />
@@ -86,11 +21,6 @@ function App() {
                 </div>
             </div>
         );
-    // return (
-    //
-    //
-    //
-    // );
 }
 
 export default App;
--- src/pages/Weather/WeatherPage.jsx
+++ src/pages/Weather/WeatherPage.jsx
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './WeatherPage.css'
 
-const WeatherPage = (props) => {
-    return (
-        <div>
-            <h2>Прогноз погоды</h2>
+const WeatherPage = () => {
 
-        </div>
-    );
+    const API_KEY = '317147e078c54d6c882102156230710'
+    const [city, SetCity] = useState('')
+    const [weather, setWeather] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+    const [isActive, setIsActive] = useState(false)
+    const [gradus,setGradus] = useState ("")
+    const [wind_kph,setWind_kph] = useState ("")
+    const [ wind_degree,setWind_degree] = useState("")
+    const [errorText  , setErrorText]  = useState("")
 
-        const { SetCity,
-            getWeather,
-            weather,
-            isActive,
-            resetWeather,
-            city,
-            gradus,
-            wind_kph,
-            wind_degree,
-            errorText
-        } = props
+    const defaultState = () => {
+        setWeather(null)
+        setIsActive (false)
+        SetCity("")
+        setGradus('')
+        setWind_kph('')
+        setWind_degree('')
+        setErrorText ('')
+    }
+
+    const getWeather = async () => {
+        setIsLoading(true)
+        try {
+            const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
+            const data = await response.json()
+
+            if (response.status === 200) {
+                setWeather(data)
+                setIsLoading(false)
+                setIsActive(true)
+                setGradus("°C🌡")
+                setWind_kph("Wind_kph")
+                setWind_degree('wind_degree')
+                setErrorText('')
+                setWeather(data)
+            } else if(response.status === 400 ) {
+                defaultState()
+                setErrorText('В запросе синтаксическая ошибка')
+            } else {
+                defaultState()
+                setErrorText('Произошла неизвестная ошибка')
+            }
+        } catch (e) {
+            defaultState()
+            setErrorText('Сервер недоступен!')
+        } }
+
+    const resetWeather = async ()  => {
+        defaultState()
+    }
 
         return (
 
@@ -33,18 +66,22 @@ const WeatherPage = (props) => {
                         <div className="nazvanie">
                             <p className='country'>{isActive && weather.location.country} </p>
                             <p className="name">{isActive && weather.location.name }</p>
+                            <p className='temp'>{isActive && weather.current.temp_c }{gradus}</p>
+
                         </div>
                         <div className="error_parent">
                             <p className="error">{errorText }</p>
                         </div>
                         <div className="temp_parent">
 
-                            <p className='temp'>{isActive && weather.current.temp_c }{gradus}</p>
 
-
+                            </div>
+                        <div className="weather-result">
                             <div className="wind_kph_parent"><p className="wind_kph">{wind_kph} { isActive && weather.current.wind_kph }</p>
                                 <p className="degree">{wind_degree} {isActive  && weather.current.wind_degree }</p>
-                            </div>
+                        </div>
+
+
                         </div>
 
                     </div>
